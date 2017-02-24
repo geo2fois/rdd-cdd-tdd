f2dd436523392b4da15f73e40525a6caf6f04cc0
geo2fois <geoffrey.roux@devinci.fr>
2017-02-24 15:54:28 +0100

    Add commented code final

--- weather/index.js
+++ weather/index.js
@@ -9,13 +9,14 @@ module.exports = (opts, callback) => {
 	opts = opts || [];
 
 	let query;
-
+    
 	if (_.isEmpty(opts)) {
 		query = new YQL('select * from weather.forecast where woeid in (select woeid from geo.places(1) where text="Dhaka, Bangladesh")');
 	} else {
 		query = new YQL('select * from weather.forecast where woeid in (select woeid from geo.places(1) where text="' + opts[0] + ', ' + opts[1] + '")');
 	}
 
+    // YQL query execution
 	query.exec((err, response) => {
 		if (err) {
 			return callback(err);
